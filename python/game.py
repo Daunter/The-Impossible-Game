f74e576b8a2e04b62975097c145b73349a8bd9a5
L2F <l2f@l2f-desktop.(none)>
2010-05-17 23:08:03 +0100

    some movemente in game.py

--- python/game.py
+++ python/game.py
@@ -2,10 +2,38 @@
 
 import pygame
 import os
+from datetime import datetime, timedelta
 
 class Drawable:
     def draw_into(self, screen, x = 0, y = 0):
         screen.blit(self.image, self.rect.move(x, y))
+
+class Box(Drawable):
+    def __init__(self, image, x, y):
+        self.jumping = False
+        self.x = 0
+        self.y = 0
+        self.j_x = 0
+        self.jump_max = 60
+        self.jump_time = timedelta(seconds = 5)
+
+    def jump(self):
+        if not self.jumping:
+            self.jumping = True
+            self.jump_start = datetime.now()
+            self.jump_peek = self.jump_start + self.jump_time / 2
+            self.jump_end = self.jump_start + self.jump_time
+
+    def draw_into(self, screen, x = 0, y = 0):
+        now = datetime.now()
+        if self.jumping:
+            if now < self.jump_peek:
+                m = now - self.jump_started
+                t = m.seconds * 1000000 + m.microseconds
+                
+    
+        if datetime.now() > self.jump_end:
+            self.jumping = False
 
 
 class Background(Drawable):
